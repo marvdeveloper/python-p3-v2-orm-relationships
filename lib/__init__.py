382acd60a21a1f18f522f4be245a1015efa1e513
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-26 23:56:17 +0300

    Add lib package initializer

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,5 @@
-# lib/__init__.py
 import sqlite3
 
-CONN = sqlite3.connect('company.db')
+CONN = sqlite3.connect('company.db', timeout=30, check_same_thread=False)
+CONN.execute("PRAGMA foreign_keys = ON;")
 CURSOR = CONN.cursor()
